53b8d6de148bed8e0a5338ee2eafe5d42311d6ea
Mohammed Joda <silicco@gmail.com>
2023-07-16 13:22:24 +0100

    update VillaDTO.cs

--- MagicVilla_webAPI/Models/DTO/VillaDTO.cs
+++ MagicVilla_webAPI/Models/DTO/VillaDTO.cs
@@ -10,6 +10,11 @@ public class VillaDTO
     [MaxLength(30)]
     public string Name { get; set; }
 
+    public string Details { get; set; }
+    public double Rate { get; set; }
+    
+    public string ImageUrl { get; set; }
+    public string Amenity { get; set; }
     public int  Occupancy { get; set; }
     public int Sqft { get; set; }
 }
